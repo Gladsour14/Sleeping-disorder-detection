96435a0fd12e9c5f0a4901344062d9c630b08c3e
Nancy <145664237+Gladsour14@users.noreply.github.com>
2024-12-30 20:16:27 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,18 @@
+import streamlit as st
+st.title("Sleep Disorder Prediction!")
+
+st.markdown('''Are you experiencing difficulties with your sleep patterns? Do you often find yourself struggling to fall asleep, waking up frequently during the night, or feeling unrested even after a full night's sleep? 
+If so, you may be dealing with a sleep disorder.  
+  
+Experience the power of sleep disorder prediction! Input your sleep habits and key lifestyle factors, and unlock personalized insights into potential sleep disorders such as insomnia and sleep apnea. 
+Take control of your sleep health and embark on a journey towards better rest and well-being.''')
+
+st.image('sleep_disorder.jpg', width=500)
+st.header('Our motive')
+st.markdown('''By leveraging the power of technology and data-driven insights, Sleep Disorder Prediction aims to empower 
+individuals like you to take proactive steps towards improving your sleep health and overall well-being. Whether 
+it's seeking professional medical advice, adopting healthier sleep habits, 
+or exploring alternative therapies, our platform serves as a valuable tool in your journey towards better sleep. 
+   
+Take the first step towards a more restful and rejuvenating sleep experience. Explore Sleep Disorder 
+Prediction today and gain valuable insights into your sleep health!''')
